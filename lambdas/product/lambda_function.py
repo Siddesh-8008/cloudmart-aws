0b64e51376e5bd1c65ae62ec08a6509a616a7039
siddesh <siddeshravula8008@gmail.com>
2026-08-27 18:01:13 +0530

    iAm10

--- lambdas/product/lambda_function.py
+++ lambdas/product/lambda_function.py
@@ -1,1 +1,22 @@
+import json
+import os
 
+
+def lambda_handler(event, context):
+    print(json.dumps({
+        "level": "INFO",
+        "message": "Product Lambda invoked",
+        "environment": os.environ.get("ENVIRONMENT"),
+        "request_id": context.aws_request_id
+    }))
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Product API is working",
+            "requestId": context.aws_request_id
+        })
+    }
